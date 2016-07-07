54289624c0b2be8ddacae19b5b7e6637ea3fdaf8
Laura Pedroni <laurage_@hotmail.com>
2016-07-07 15:21:07 +0200

    cleaing

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,13 +1,37 @@
 class IngredientsController < ApplicationController
+  # Create
   def new
   end
 
+  def create
+  end
+
+  # Read
   def index
   end
 
   def show
   end
 
+  # Update
   def edit
+  end
+
+  def update
+  end
+
+  # Delete
+  def destroy
+  end
+
+  # Add a dose
+  def newDose
+  end
+
+  def createDose
+  end
+
+  # Delete a dose
+  def deleteDose
   end
 end
