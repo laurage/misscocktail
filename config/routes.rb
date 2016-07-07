7f5ebfe5fa858169533341520b73ae730e17cf5b
Laura Pedroni <laurage_@hotmail.com>
2016-07-07 14:56:16 +0200

    table cocktails created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'ingredients/new'
+
+  get 'ingredients/index'
+
+  get 'ingredients/show'
+
+  get 'ingredients/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
